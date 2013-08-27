06b43e13704096cfb7e971d4709bd183287c10bc
Brandon Schabel <brandonschabel@yahoo.com>
2013-08-27 15:50:00 -0700

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,16 @@
+class Owner
+
+	def name
+		name = 'Foobar Kadigan'
+	end
+
+	def birthdate
+		birthdate = Date.new(1990, 9, 22)
+	end
+
+	def countdown
+		today = Date.today
+		birthday = Date.new(today.year, self.birthdate.month, self.birthdate.day)
+		countdown = (birthday - today).to_i
+	end
+end
